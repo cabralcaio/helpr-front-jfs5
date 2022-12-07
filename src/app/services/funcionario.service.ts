6d3fc2308c8ace94d1497f0b667e2387a6db7aa1
jnnixx <jennifer.fernanda699@gmail.com>
2022-12-07 10:06:39 -0300

    foi realizado o service de funcionario

--- src/app/services/funcionario.service.ts
+++ src/app/services/funcionario.service.ts
@@ -4,14 +4,18 @@ import { catchError, EMPTY, Observable } from 'rxjs';
 import { API_CONFIG } from '../config/api.config';
 import { Funcionario } from '../models/funcionario';
 
+//task #14 foi realizada na task anterior #13
+
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionarioService {
 
+
   constructor(
     private http: HttpClient
   ) { }
+
 
   public findAll(): Observable<Funcionario[]>{
     return this.http.get<Funcionario[]>(`${API_CONFIG.baseUrl}/funcionarios`).pipe(
